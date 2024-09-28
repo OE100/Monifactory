f6d26b7fa7a5970fff8ae0f48ad8ea629f88e87b
OE100 <omer.er10@gmail.com>
2024-09-28 14:23:40 +0300

    added fun suspicious record recipe

--- kubejs/server_scripts/Fun_Recipes.js
+++ kubejs/server_scripts/Fun_Recipes.js
@@ -1,3 +1,13 @@
 ServerEvents.recipes(event => {
-
+    // pretty sus if you ask me
+    event.recipes.shaped(
+        'gtceu:sus_record', [
+            'SSS',
+            'SRS',
+            'SSS'
+        ], {
+            R: 'gtceu:polyvinyl_chloride_ring',
+            S: Item.of('minecraft:written_book', { display: { Name : '{"text":"ඞ"}'}}).weakNBT()
+        }
+    ).id('gtceu:sus_record')
 })
